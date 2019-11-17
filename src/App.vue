e4afd181d41296a6a833aed85e57c8eed5f74594
Stephane P. Pericat <contact@pericat.net>
2019-11-17 14:28:46 -0600

    comments

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,28 @@ import { reactive, computed, watch, onMounted } from "@vue/composition-api";
 
 export default {
   setup() {
+    /**
+     * Initial State
+     */
     const state = reactive({
       count: 0
     });
 
+    /**
+     * Computed Properties
+     */
     const double = computed(() => state.count * 2);
 
+    /**
+     * Methods
+     */
     function increment() {
       state.count++;
     }
 
+    /**
+     * Watchers
+     */
     watch(() => {
       console.log("COUNT >>", state.count);
     });
@@ -26,10 +38,16 @@ export default {
       console.log("DOUBLE >>>", double.value);
     });
 
+    /**
+     * Lifecycle Hooks
+     */
     onMounted(() => {
       console.log("COMPONENT MOUNTED >>>", state);
     });
 
+    /**
+     * return what the template needs...
+     */
     return {
       double,
       increment,
