ec1b7d786612d1c2171d91b44f200f4c56b3379b
Stephane P. Pericat <contact@pericat.net>
2019-11-17 14:13:36 -0600

    basic example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>count is : {{ state.count }}</div>
+  <button @click="increment">
+    Count is: {{ state.count }}, double is: {{ state.double }}
+  </button>
 </template>
 <script>
-import { reactive } from "@vue/composition-api";
+import { reactive, computed } from "@vue/composition-api";
 
 export default {
   setup() {
     const state = reactive({
-      count: 0
+      count: 0,
+      double: computed(() => state.count * 2)
     });
 
+    function increment() {
+      state.count++;
+    }
+
     return {
-      state
+      state,
+      increment
     };
   }
 };
